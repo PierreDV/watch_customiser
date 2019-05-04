92c797d2b24a930d06fcfe96e41ea0831fc52822
PierreDV <p.verteuil@gmail.com>
2019-05-04 14:20:43 +0200

    update product config and render to make use of render-prop pattern

--- src/ProductConfig.js
+++ src/ProductConfig.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import Canvas from "./Canvas";
-import Color from './Color';
-import Zoom from './Zoom';
 
 class ProductConfig extends Component {
   state = {
@@ -17,13 +14,12 @@ class ProductConfig extends Component {
 
   render() {
     return (
-      <div className="product-container">
-        <Canvas colorChoice={this.state.colorChoice} zoom={this.state.zoom} />
-        <Color
-          colorChoice={this.state.colorChoice}
-          onChange={this.handleChange}
-        />
-        <Zoom zoom={this.state.zoom} onChange={this.handleChange} />
+      <div>
+        {this.props.children(
+          this.state.colorChoice,
+          this.state.zoom,
+          this.handleChange
+        )}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import "./index.css";
 import ProductConfig from './ProductConfig';
+import Color from './Color';
+import Zoom from './Zoom';
+import Canvas from "./Canvas";
 
-ReactDOM.render(<ProductConfig />, document.getElementById("root"));
+ReactDOM.render(
+  <ProductConfig>
+    {(colorChoice, zoom, handleChange) => (
+      <div className="product-container">
+        <h1>A custom header; just for fun!</h1>
+        <Color
+          colorChoice={colorChoice}
+          onChange={handleChange}
+        />
+        <Zoom zoom={zoom} onChange={handleChange} />
+        <Canvas colorChoice={colorChoice} zoom={zoom} />
+      </div>
+    )}
+  </ProductConfig>, 
+  document.getElementById("root")
+);
