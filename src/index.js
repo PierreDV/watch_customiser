29ec550013b3813182165c0f4f290628f92624f0
PierreDV <p.verteuil@gmail.com>
2019-05-04 14:01:51 +0200

    add start files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import Canvas from "./Canvas";
 
-ReactDOM.render(
-  <h1>Watch Customiser</h1>,
-  document.getElementById('root')
-);
+function Color() {
+  return null; // You'll create this in a later exercise
+}
+
+function Zoom() {
+  return null; // You'll create this in a later exercise
+}
+
+class ProductConfig extends React.Component {
+  state = {
+    colorChoice: null,
+    zoom: null
+  };
+
+  handleChange = e => {
+    let change = {};
+    change[e.target.name] = JSON.parse(e.target.value);
+    this.setState(change);
+  };
+
+  render() {
+    return (
+      <div className="product-container">
+        <Canvas colorChoice={this.state.colorChoice} zoom={this.state.zoom} />
+        <Color
+          colorChoice={this.state.colorChoice}
+          onChange={this.handleChange}
+        />
+        <Zoom zoom={this.state.zoom} onChange={this.handleChange} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProductConfig />, document.getElementById("root"));
