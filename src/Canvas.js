e2bef1f6aa2e7e1f1a562ac051db0708d0d30bbf
PierreDV <p.verteuil@gmail.com>
2019-05-10 22:50:07 +0200

    broken first draft of conversion to hooks with original code commented out for reference

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,55 +1,91 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { drawCanvas } from "./helpers";
 
-class Canvas extends Component {
-  state = {
+function Canvas(props) {
+  const canvas = useRef(null);
+
+  const [time, setTime] = useState({
     time: {
       hours: null,
       minutes: null,
       seconds: null
     }
-  };
+  });
+  // state = {
+  //   time: {
+  //     hours: null,
+  //     minutes: null,
+  //     seconds: null
+  //   }
+  // };
 
-  setTime() {
+  function createAndSetTime() {
     const d = new Date();
     const hours = d.getHours();
     const minutes = d.getMinutes();
     const seconds = d.getSeconds();
 
-    return this.setState({ time: { hours, minutes, seconds } });
+    return setTime({ time: { hours, minutes, seconds } });
   }
+  // setTime() {
+  //   const d = new Date();
+  //   const hours = d.getHours();
+  //   const minutes = d.getMinutes();
+  //   const seconds = d.getSeconds();
 
-  componentWillMount() {
-    this.setTime(
+  //   return this.setState({ time: { hours, minutes, seconds } });
+  // }
+
+  useEffect(() => {
+    createAndSetTime(
       window.setInterval(() => {
-        this.setTime();
+        createAndSetTime();
       }, 1000)
-    );
-  }
+    )
+  }, [])
 
-  componentDidMount() {
-    this.ctx = this.refs.canvas.getContext("2d");
+  // componentWillMount() {
+  //   this.setTime(
+  //     window.setInterval(() => {
+  //       this.setTime();
+  //     }, 1000)
+  //   );
+  // }
+
+  useEffect(() => {
+    const ctx = canvas.current.getContext("2d");
     drawCanvas(
-      this.ctx,
-      this.props,
-      this.state.time, // { hours, minutes, seconds }
-      this.refs.canvas.width
+      ctx,
+      props,
+      time,
+      canvas.current.width
     );
-  }
+    return () => {
+      ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+    }
+  });
 
-  componentDidUpdate(prevProps) {
-    this.ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
-    drawCanvas(
-      this.ctx,
-      this.props,
-      this.state.time, // { hours, minutes, seconds }
-      this.refs.canvas.width
-    );
-  }
+  // componentDidMount() {
+  //   this.ctx = this.refs.canvas.getContext("2d");
+  //   drawCanvas(
+  //     this.ctx,
+  //     this.props,
+  //     this.state.time, // { hours, minutes, seconds }
+  //     this.refs.canvas.width
+  //   );
+  // }
 
-  render() {
-    return <canvas ref="canvas" width={250} height={250} />;
-  }
+  // componentDidUpdate(prevProps) {
+  //   this.ctx.clearRect(0, 0, this.refs.canvas.width, this.refs.canvas.height);
+  //   drawCanvas(
+  //     this.ctx,
+  //     this.props,
+  //     this.state.time, // { hours, minutes, seconds }
+  //     this.refs.canvas.width
+  //   );
+  // }
+
+  return <canvas ref={canvas} width={250} height={250} />;
 }
 
 export default Canvas;
